03e10a23fe3c1ec28292f17eb7e3db45c5c39250
Alexander Sheltrekov <asheltrekov@yandex.ru>
2026-02-24 21:03:59 +0300

    Исправлены ошибки в конверторе валют

--- 7-converter/7-converter.js
+++ 7-converter/7-converter.js
@@ -1,12 +1,15 @@
 function convert(sum, currency1, currency2) {
-    if (currency1 === 'руб' && currency2 === '$') {
-        let res = sum / 76,75;
-        return res;
-    } else if (currency1 === 'руб' && currency2 === "eur")  {
-            let res = sum / 90,28;
-            return res;
+    if (currency1 === 'RUB' && currency2 === 'USD') {
+        let res = sum / 76.75;
+        return +res.toFixed(2);
+    } else if (currency1 === 'RUB' && currency2 === "EUR")  {
+            let res = sum / 90.28;
+            return +res.toFixed(2);
+        } else if (currency1 === 'RUB' && currency2 === "GBP") {
+            let res = sum / 103.26;
+            return +res.toFixed(2); 
         } else {
-        return null;
+            return null;
         }
 }
-console.log(convert(50000, "руб", "$"));
+console.log(convert(50000, "RUB", "USD"));
